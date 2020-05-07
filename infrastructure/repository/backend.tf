cb9ee79950014870c4159f4ce20bb5b607335f50
Brian Flad <bflad417@gmail.com>
2020-05-07 12:13:19 -0400

    Initial infrastructure/repository testing

---
+++ infrastructure/repository/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "atlas" {
+    name = "hashicorp-v2/terraform-provider-aws-repository"
+  }
+
+  required_providers {
+    github = "2.1.0"
+  }
+
+  required_version = "~> 0.12.2"
+}
